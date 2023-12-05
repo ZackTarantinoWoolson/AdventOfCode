4e84b8eded9d19b08002d9fefd7eb52ad1ca93ff
ZackTarantinoWoolson <15880559+ZackTarantinoWoolson@users.noreply.github.com>
2023-12-05 18:14:29 -0500

    counting cards should be working, nested loop

--- Day4_Scratchcards/sractchcards.rb
+++ Day4_Scratchcards/sractchcards.rb
@@ -1,6 +1,6 @@
 Dir.chdir(File.dirname(__FILE__))
-input=File.open("puzzleInput.txt").readlines.map(&:chomp)
-# input=File.open("teststrings.txt").readlines.map(&:chomp)
+# $input=File.open("puzzle$input.txt").readlines.map(&:chomp)
+$input=File.open("teststrings.txt").readlines.map(&:chomp)
 
 
 def get_winning_numbers(line)
@@ -24,10 +24,29 @@ def calculate_points(wins)
     points
 end
 
+def calculate_card_instances(wins,ind)
+    new_cards=Array.new
+    # p "number of wins: #{wins} for card #{ind}"
+    (1..wins).each do |x|
+        new_cards<<ind+x
+    end
+
+    new_cards
+end
+
+def preset_game_ids
+    hs=Hash.new
+    $input.each do |line|
+        game_id= line.split(": ")[0].split(" ")[1]
+        hs[game_id.to_i]=1
+    end
+    hs
+end
+
 ### Part 1: 
 total_points=0
 
-input.each do |line|
+$input.each do |line|
     wins=0
     game_array=Array.new
     game_id= line.split(": ")[0].split(" ")[1]
@@ -44,19 +63,39 @@ end
 
 p "Part 1: #{total_points}"
 
-input.each do |line|
-    wins=0
+### Part 2:
+
+total_instances=preset_game_ids
+$input.each_with_index do |line,ind|
+    
+    
     game_array=Array.new
     game_id= line.split(": ")[0].split(" ")[1]
 
-    winning_numbers= get_winning_numbers(line)
+    # p total_instances[game_id.to_i]
 
-    get_play_numbers(line).each do |num|
-        if winning_numbers.include?(num)
-            #card instances
+    (1..total_instances[game_id.to_i]).each do |x|
+        wins=0
+
+        # total_instances[game_id.to_i]=1
+        # p total_instances
+
+        winning_numbers= get_winning_numbers(line)
+
+        get_play_numbers(line).each do |num|
+            if winning_numbers.include?(num)
+                wins+=1
+            end
+        end
+
+        calculate_card_instances(wins,ind+1).each do |game|
+            # p "game: #{game}"
+            total_instances[game]+=1 unless game>total_instances.keys.last
         end
     end
 end
+
+total_instances.each {|x| total+=x}
 
 
 # p "Part 2: #{product}"
