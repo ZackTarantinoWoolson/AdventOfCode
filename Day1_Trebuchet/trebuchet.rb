138e41e9ba85ba979ef17242fb932477ce58f377
ZackTarantinoWoolson <15880559+ZackTarantinoWoolson@users.noreply.github.com>
2023-12-04 19:52:05 -0500

    find numbers per line is done ?

--- Day1_Trebuchet/trebuchet.rb
+++ Day1_Trebuchet/trebuchet.rb
@@ -7,28 +7,24 @@ test_strings = ["kjrqmzv9mmtxhgvsevenhvq7"]
 Dir.chdir(File.dirname(__FILE__))
 input=File.open("puzzleInput.txt").readlines.map(&:chomp)
 
-$num_strings = ["zero","one","two","three","four","five","six","seven","eight","nine"]
-$nums = ["0","1","2","3","4","5","6","7","8","9"]
+$num_strings = ["zero","one","two","three","four","five","six","seven","eight","nine",
+    "0","1","2","3","4","5","6","7","8","9"]
+# $nums = ["0","1","2","3","4","5","6","7","8","9"]
 
 def find_numbers(line)
-    num_index = Hash.new
+    num_index = Hash.new # {}"num" => index}
     $num_strings.each do |num|
         ind = line.index(num)
         unless ind.nil?
             num_index[num]=ind
         end
     end
-    $nums.each do |num|
-        ind = line.index(num)
-        unless ind.nil?
-            num_index[num]=ind
-        end
-    end
-
-    p num_index.sort_by{|k,v| v}
+    num_index.sort_by{|k,v| v}
 end
 
 test_strings.each do |line|
     p line
     nums_in_line = find_numbers(line)
+
+    p "first #{nums_in_line.first} | last: #{nums_in_line.last}"
 end
