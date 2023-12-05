7e29d7c47f0d03087a77dcb577a34fe5e2ea3c8c
ZackTarantinoWoolson <15880559+ZackTarantinoWoolson@users.noreply.github.com>
2023-12-04 20:10:09 -0500

    all tasks complete - testing

--- Day1_Trebuchet/trebuchet.rb
+++ Day1_Trebuchet/trebuchet.rb
@@ -1,15 +1,17 @@
 # test_strings = ["kjrqmzv9mmtxhgvsevenhvq7","four2tszbgmxpbvninebxns6nineqbqzgjpmpqr",
 # "rkzlnmzgnk91zckqprrptnthreefourtwo","fouronevzkbnzm6seven47",
 # "zphgdcznqsm2","4gjnmxtrbflgp71","4sqvv1cnpn"]
-test_strings = ["kjrqmzv9mmtxhgvsevenhvq7"]
+
+test_strings=["1abc2","pqr3stu8vwx","a1b2c3d4e5f","treb7uchet"]
+
+# test_strings = ["seven5trmnqnqgqprkdkxcsrtmzjccgrtbklnhnfive"]
 
 
 Dir.chdir(File.dirname(__FILE__))
 input=File.open("puzzleInput.txt").readlines.map(&:chomp)
 
-$num_strings = ["zero","one","two","three","four","five","six","seven","eight","nine",
-    "0","1","2","3","4","5","6","7","8","9"]
-# $nums = ["0","1","2","3","4","5","6","7","8","9"]
+$num_strings = ["zero","one","two","three","four","five","six","seven","eight","nine"]
+$nums = ["0","1","2","3","4","5","6","7","8","9"]
 
 def find_numbers(line)
     num_index = Hash.new # {}"num" => index}
@@ -19,22 +21,36 @@ def find_numbers(line)
             num_index[num]=ind
         end
     end
+    $nums.each do |num|
+        ind = line.index(num)
+        unless ind.nil?
+            num_index[num]=ind
+        end
+    end
     num_index.sort_by{|k,v| v}
 end
 
-def convert_strings(val)
+def convert_strings(num)
+    if($nums.include?(num[0]))
+        return num[0]
+    end
     $num_strings.each_with_index do |ele,index|
-        p index
+        if (num[0]==ele)
+            return index
+        end
     end
 end
+
+final_num = 0
 
 test_strings.each do |line|
-    p line
+    # p line
     nums_in_line = find_numbers(line)
-
-    nums_in_line.each do |num|
-        convert_strings(num)
-    end
-
     # p "first #{nums_in_line.first} | last: #{nums_in_line.last}"
+
+    sub_total = convert_strings(nums_in_line.first).to_s
+    sub_total += convert_strings(nums_in_line.last).to_s
+    final_num += sub_total.to_i
 end
+
+p final_num
