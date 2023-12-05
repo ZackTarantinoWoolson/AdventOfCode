39d9bfd99e2d619d03b4f4c076626e5f369125e8
zachary-tarantinowoolson-sp <zachary.tarantino@sailpoint.com>
2023-12-05 12:30:40 -0500

    had to -1 the lengh condition in find_neighbors

--- Day3_Gear_Ratios/gearratios.rb
+++ Day3_Gear_Ratios/gearratios.rb
@@ -24,15 +24,12 @@ def find_number_col(line)
         # p ind, ch
         unless ch.match(/[0-9]/).nil?
             num = is_next_char_num(line,ind)
-            num_list << [num,ind]
+            num_list << [num,ind+1] #increas index by 1 to make the it index1 instead of index0
             # p num
             ind+=num.length
         else
             ind+=1
         end
-
-        # ind+=num.length
-        # ind+=1
         num=""
         break if ind+1>line.length
     end
@@ -59,6 +56,23 @@ def find_symbol_col(line)
     sym_list.sort_by{|k,v| v}
 end
 
+### MIGHT HAVE TO REVISIT THIS- If a number is above 2 symbols, it counts twice right now.
+def find_neighbors(row,col,nums)
+    # p nums
+    # p "finding in #{row}"
+    line_total=0
+    nums[1].each do |n|
+        # p "NUmber: #{n[0]} | numbers index: #{n[1]} | Col: #{col}"
+        if (([col-1,col,col+1].include?(n[1])) || ([col-1,col,col+1].include?(n[1]+n[0].length.to_i-1)))
+            # p "#{n[0]} is in"
+            line_total+=n[0].to_i
+        end
+    end
+
+    return line_total
+end
+
+
 sym_table = Array.new
 num_table = Array.new
 
@@ -75,5 +89,19 @@ input.each_with_index do |line,ind|
 end
 
 p sym_table
-p num_table
+p num_table,""
 
+all_numbers=Array.new
+
+sym_table.each do |s|
+    row = s[0]
+    cols= s[1]
+    # p row,cols
+    cols.each do |c|
+        all_numbers<<find_neighbors(row-1,c,num_table[row-1]) unless num_table[row-1].nil?
+        all_numbers<<find_neighbors(row,c,num_table[row])
+        all_numbers<<find_neighbors(row+1,c,num_table[row+1]) unless num_table[row+1].nil?
+    end
+end
+
+p all_numbers,all_numbers.sum
