2a9228cb22396040ef7e74d36a76a4dae9c72c19
zachary-tarantinowoolson-sp <zachary.tarantino@sailpoint.com>
2023-12-05 10:45:03 -0500

    got a table of row and col for symbols

--- Day3_Gear_Ratios/gearratios.rb
+++ Day3_Gear_Ratios/gearratios.rb
@@ -2,25 +2,38 @@ Dir.chdir(File.dirname(__FILE__))
 # input=File.open("puzzleInput.txt").readlines.map(&:chomp)
 input=File.open("teststrings.txt").readlines.map(&:chomp)
 
-$symbols = ["!","@","#","$","%","^","&","*","(",")","-","=","_","+","<",">","?"]
+$symbols = ["!","@","#","$","%","&","*","-","=","_","+","<",">","?"]
 
 # Reused from day 1
 def scan_string(string, regex)
     result = Array.new
     (0..string.length).each do |x|
-        if (string[x..-1] =~ /^#{regex}/)
+        if (string[x-1] =~ /[#{regex}]/)
             result << [Regexp.last_match.to_s,x] 
         end
     end
     result
 end
 
-def find_symbol()
-
+def find_symbol_col(line,ind)
+    sym_list = Array.new
+    $symbols.each do |sym|
+        scan = scan_string(line,sym)
+        scan.each { |s| sym_list<< s[1] } unless (scan.empty?)
+    end
+    sym_list.sort_by{|k,v| v}
 end
 
+sym_table=Array.new
 
-input.each do |line|
+input.each_with_index do |line,ind|
     p line
+    rc=Array.new # row and collumn of symbols
 
+    rc<<ind
+    rc<<find_symbol_col(line,ind)
+
+    sym_table<<rc
 end
+
+p sym_table
