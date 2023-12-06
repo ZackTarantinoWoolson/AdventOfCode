81d75b0596845243f4fd053c659c726ac0684ed5
ZackTarantinoWoolson <15880559+ZackTarantinoWoolson@users.noreply.github.com>
2023-12-06 18:46:47 -0500

    day6 init

---
+++ Day6_WaitForIt/DaySix.md
@@ -0,0 +1,25 @@
+
+
+# Notes
+
+
+## Part 1
+
+Start: 
+Finished 
+
+End result: 
+
+Tasks:
+- 
+
+## Part 2
+
+Start: 
+Finished  
+
+Result needed: 
+
+
+Tasks:
+- 
---
+++ Day6_WaitForIt/waitforit.rb
@@ -0,0 +1,3 @@
+Dir.chdir(File.dirname(__FILE__))
+$input=File.open("puzzleInput.txt").readlines.map(&:chomp)
+# $input=File.open("teststrings.txt").readlines.map(&:chomp)
